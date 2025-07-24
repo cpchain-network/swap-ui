1b40f5c181b319620e72afc4486572fbdbbe96da
guoshijiang <guoshijiang2012@163.com>
2025-07-24 17:53:28 +0800

    add swap deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,9 @@ export default defineConfig({
     host: '0.0.0.0', // 允许外部访问，通常用于 Docker 或云环境
     port: 3000,      // 配置服务器监听端口
     open: true,      // 自动打开浏览器
-   
+    allowedHosts: [
+      'swap-testnet.cpchain.com', // 添加你要允许的 host
+    ],
   },
   build: {
     assetsInlineLimit: 4096, //  小于 4kb 才 base64
