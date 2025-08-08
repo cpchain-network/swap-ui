023f9ef248861c5d2353beb0c2857dcf1e77f097
guoshijiang <guoshijiang2012@163.com>
2025-08-08 14:47:41 +0800

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     port: 3000,      // 配置服务器监听端口
     open: true,      // 自动打开浏览器
     allowedHosts: [
+      'swap.cpchain.com',
       'swap-testnet.cpchain.com', // 添加你要允许的 host
     ],
   },
